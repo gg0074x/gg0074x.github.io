4838b7ac355495738b4f05d7a2f7aa1b0bf19b5d
GG0074X <74442646+GG0074X@users.noreply.github.com>
2022-10-04 22:27:24 -0300

    Babeeee

---
+++ playsound.js
@@ -0,0 +1,14 @@
+var sounds = [
+     {s: new Audio('s1.mp3')},
+     {s: new Audio('s2.mp3')},
+     {s: new Audio('s3.mp3')},
+     {s: new Audio('s4.mp3')},
+     {s: new Audio('s5.mp3')},
+     {s: new Audio('s6.mp3')},
+     {s: new Audio('s7.mp3')}
+];
+
+function sound(){
+     sounds[1].s.play()//plays the sound
+     console.log("babe")
+}
