cbce5bb54fe2a8813e6c9e58c0c99f6b432886d9
Chiwa <74442646+gg0074x@users.noreply.github.com>
2024-12-11 15:48:16 -0300

    feat: added new component

---
+++ src/components/Tech.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps<{
+  title: string,
+}>();
+</script>
+
+<template>
+  <div class="tech-container">
+    <span class="dot"></span>
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<style scoped>
+.dot {
+  height: 8px;
+  width: 8px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+}
+.tech-container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5% 0;
+  width: 100%;
+  gap: 2%;
+}
+
+/* LANGUAGE CLASSES OR SOMETHING */
+.rust .dot {
+  background-color: rgb(var(--rust));
+}
+.vue .dot {
+  background-color: rgb(var(--vue));
+}
+</style>
